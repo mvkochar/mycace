2ca6c54e143c62284c4f849124866803acb9222f
sgkochar <sgkochar@gmail.com>
2024-02-10 18:48:21 +0200

    create ShopRegistration page

--- src/pages/ShopRegistration.tsx
+++ src/pages/ShopRegistration.tsx
@@ -12,6 +12,7 @@ const ShopRegistration = ({ handleHeaderType }: ShopRegistrationProps) => {
     }, []);
 
     return (
+
         <main>
             <div className="wrapper">
                 <div className="shop-reg-main d-f">
@@ -24,6 +25,65 @@ const ShopRegistration = ({ handleHeaderType }: ShopRegistrationProps) => {
                         </div>
                         <div className='shop-reg-polygon'><img src="images/polygon.svg" alt="polygon" /></div>
                     </div>
+                    <form action="" className='shop-reg-fm d-f'>
+                        <div>
+                            <h4 className="form-bl-title">Фото</h4>
+                            <div className="form-bl-desc">
+                                Додайте сюди своє фото або логотип компанії. Кондитерів з фото обирають частіше
+                            </div>
+                            <button type='button' className='add-photo-btn'>Завантажити фото</button>
+                        </div>
+                        <div>
+                            <h4 className="form-bl-title">Загальне</h4>
+                            <label htmlFor='confectioner' className="form-bl-label">Ім’я кондитера або назва кондитерської</label>
+                            <input type="text" name='confectioner' id='confectioner' placeholder='Інна Пономаренко' />
+                            <label htmlFor='desc' className="form-bl-label">Опис</label>
+                            <p className="form-bl-desc">
+                                Допоможіть клієнтам більше дізнатися про вас. Тут можна написати про свій досвід,
+                                навички та досягнення.
+                            </p>
+                            <textarea name="desc" id="desc"></textarea>
+                        </div>
+                        <div>
+                            <h4 className="form-bl-title">Контакти</h4>
+                            <label htmlFor="address" className='form-bl-label'>Адреса</label>
+                            <input type="text" name='address' id='address' />
+                            <label htmlFor="phone" className='form-bl-label'>Номер телфону</label>
+                            <input type="tel" name="phone" id="phone" placeholder='+56 085 345 76 34' />
+                            <label htmlFor="mail" className='form-bl-label'>Пошта</label>
+                            <input type="text" name='mail' id='mail' />
+                            <button type='button' className='add-contact-btn'>Додати контакт</button>
+                        </div>
+                        <div>
+                            <h4 className="form-bl-title">Доставка</h4>
+                            <p className="form-bl-desc">
+                                Опишіть доступні способи доставки, їх вартість та умови. <br />
+                                Наприклад: “Власна доставка, безкоштовно для замовлення від 1000 грн”, “Таксі за тарифами перевізника”
+                            </p>
+                            <textarea name="delivery" id="delivery"></textarea>
+                        </div>
+                        <div>
+                            <h4 className="form-bl-title">Оплата</h4>
+                            <p className="form-bl-desc">
+                                Опишіть доступні способи оплати. <br />
+                                Наприклад: “Передплата 30%”, “Оплата терміналом кур’єру”
+                            </p>
+                            <input type="text" name="payment" id="payment"></input>
+                        </div>
+                        <div>
+                            <h4 className="form-bl-title">Соціальні мережі</h4>
+                            <p className="form-bl-desc">
+                                Вставте посилання на свої соцмережі в цих полях
+                            </p>
+                            <label htmlFor="instagram" className='form-bl-label'>Instagram</label>
+                            <input type="text" name='instagram' id='instagram' />
+                            <label htmlFor="facebook" className='form-bl-label'>Facebook</label>
+                            <input type="text" name='facebook' id='facebook' />
+                            <label htmlFor="youtube" className='form-bl-label'>Youtube</label>
+                            <input type="text" name='youtube' id='youtube' />
+                        </div>
+                        <button type='button' className='save-btn'>Зберегти</button>
+                    </form>
                 </div>
             </div>
         </main>
